e0da83a2b771bce579d41812c7b259e61c7dc997
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-22 18:26:58 +0200

    specific id of an animal is passed through using useParams :)

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<HomePage />} />
-      <Route path=":type" element={ < HomePage />} /> 
+      <Route path=":type" element={ < HomePage />} /> {/* I AM UNSURE ABOUT THIS */}
+      <Route path=":type/:id" element={ < PetDetailsPage />} /> 
     </Route>
   )
 );
