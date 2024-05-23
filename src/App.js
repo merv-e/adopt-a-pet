4d60c9a02fa5fad8e5d193d789b94c95b0af206f
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-23 17:19:44 +0200

    SearchPage  is added to the Routes.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const appRouter = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path=":type" element={ < HomePage />} /> {/* I AM UNSURE ABOUT THIS */}
       <Route path=":type/:id" element={ < PetDetailsPage />} /> 
+      <Route path="search" element={<SearchPage />} />
     </Route>
   )
 );
